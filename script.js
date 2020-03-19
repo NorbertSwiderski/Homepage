3fdcedc69e975e580614d5c3bd6591dfd7eb334a
Norbert Świderski <norbert.swiderski@gmail.com>
2020-03-19 12:53:12 +0100

    feat: new navigation with flexbox

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 console.log('Cześć i czołem! ;)');
 const paragraph = document.querySelector('.paragraph--js');
 paragraph.innerHTML= 'Testowy paragraf JS';
+function nameAndAge(name, age){
+    console.log(`Mam na imię ${name}. Mam ${age} lat`);
+}
+nameAndAge('Norbert', 21);
